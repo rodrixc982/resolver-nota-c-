71331e1032531c358d603ecf0f76a7e9a2e2b257
rodrixc982 <cristian.rodrixc982@gmail.com>
2023-07-09 13:28:20 -0500

    resolviendo notas de estudiante en c#

---
+++ Project2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarea06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la primera nota:");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda nota:");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double promedio = (nota1 * 2 + nota2 * 3) / 5;
+
+            Console.WriteLine("El promedio ponderado de las notas es: " + promedio.ToString());
+
+            Console.WriteLine("Presione Enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
+
